4c413087bce692511c16acc5f2e443b5bdd4b58f
MightyTechNinja <brianwhitedev1996@outlook.com>
2024-06-12 07:59:30 -0700

    Deleted Ignition Folder

--- ignition/modules/Token.js
+++
@@ -1,9 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const TokenModule = buildModule("TokenModule", (m) => {
-  const token = m.contract("Token");
-
-  return { token };
-});
-
-module.exports = TokenModule;
